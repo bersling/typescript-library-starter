253cc088c87a00bc806942c839db874180116db4
Michael Smyers <michael@smyers.net>
2022-11-03 19:42:36 -0700

    lazy

--- packages/ethers-money/src/types/index.ts
+++ packages/ethers-money/src/types/index.ts
@@ -1,3 +1,22 @@
-export type Lazy<T> = T | Promise<T>;
+import {isPromise} from "util/types";
 
 export {Numbers} from "./Numbers"
+
+//region Lazy
+export type Lazy<T> = T | Promise<T>;
+
+/**
+ * if lazy is a promise, then executes async (".then")
+ * if lazy is an object, then executes sync (immediately)
+ *
+ * @param lazy
+ * @param fn
+ */
+export function laze<T>(lazy: Lazy<T>, fn: (item: T) => Lazy<T>): Lazy<T> {
+    if (isPromise(lazy)) {
+        return lazy.then((item) => fn(item))
+    } else {
+        return fn(lazy as T)
+    }
+}
+//endregion
